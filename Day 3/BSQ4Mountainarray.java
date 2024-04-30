0f20c72f43eb96abe0c2c5f6850198b68b5383b1
Gaurav Shah <100478378+Gauravshah001@users.noreply.github.com>
2024-04-30 22:51:57 +0530

    Update BSQ4Mountainarray.java

--- Day 3/BSQ4Mountainarray.java
+++ Day 3/BSQ4Mountainarray.java
@@ -11,7 +11,7 @@ public class BSQ4Mountainarray {
     public int peakIndexInMountainArray(int[] arr) {
     int start=0;
     int end=arr.length-1;
-    while(start<=end){
+    while(start<end){
         int mid=start+(end-start)/2;
         if(arr[mid]>arr[mid+1]){
             //dec part of array look at left
